e33c54e8d9dab94e84ef3a40030bb00748d85edc
David garzón <dgv241198@gmail.com>
2019-03-21 13:06:45 -0500

    Uso de permisos

--- Components/SeleccionarImagen.js
+++ Components/SeleccionarImagen.js
@@ -1,18 +1,40 @@
 import React from 'react';
-import { Button, Image, View, TouchableOpacity } from 'react-native';
-import { ImagePicker } from 'expo';
+import { Button, Image, View, TouchableOpacity, Alert } from 'react-native';
+// import { ImagePicker } from 'expo';
+import { ImagePicker, Permissions } from 'expo';
 
 const SeleccionarImagen = (props)=>{
+
+    const permiso = async () =>{
+        const response = await Permissions.getAsync(Permissions.CAMERA_ROLL);
+        return response;
+    }
+
     const seleccionarImagen = async () => {
-        let result = await ImagePicker.launchImageLibraryAsync({
-            allowsEditing: true,
-            aspect: [4, 3],
-        });
-        // console.log(result);
-        if (!result.cancelled) {
-            // this.setState({ image: result });
-            //propiedad que hace el dispatch, enviada desde SignUp
-            props.cargar(result);
+        const respuestaPermiso = await permiso();
+        //En caso de no tener el permiso, preguntar
+        if(respuestaPermiso.status == "undetermined"){
+            await Permissions.askAsync(Permissions.CAMERA_ROLL);
+        }else if(respuestaPermiso.status == "denied"){
+            Alert.alert('Error',"Debes otorgar los permisos correspondientes",[{
+                text:"Otorgar permisos",
+                onPress: ()=>{
+                    Permissions.askAsync(Permissions.CAMERA_ROLL);
+                }
+            },{
+                text:"Cancelar",
+            }]);
+        }else if(respuestaPermiso.status == "granted"){
+            let result = await ImagePicker.launchImageLibraryAsync({
+                allowsEditing: true,
+                aspect: [4, 3],
+            });
+            // console.log(result);
+            if (!result.cancelled) {
+                // this.setState({ image: result });
+                //propiedad que hace el dispatch, enviada desde SignUp
+                props.cargar(result);
+            }
         }
     };
     //Propiedad inyectada en seleccionar galería
